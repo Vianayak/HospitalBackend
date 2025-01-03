f611fc965c10f18baf03ba7cd0a95af79c3eeb43
vinayak banoth <code.editor>
2025-01-03 12:01:29 +0530

    Added Date and Time table for book appointment

---
+++ src/main/java/com/hospital/model/DateAndTimeInfo.java
@@ -0,0 +1,46 @@
+package com.hospital.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class DateAndTimeInfo {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private int regestrationNum;
+private String date;
+private String slot;
+private String time;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getRegestrationNum() {
+	return regestrationNum;
+}
+public void setRegestrationNum(int regestrationNum) {
+	this.regestrationNum = regestrationNum;
+}
+public String getDate() {
+	return date;
+}
+public void setDate(String date) {
+	this.date = date;
+}
+public String getSlot() {
+	return slot;
+}
+public void setSlot(String slot) {
+	this.slot = slot;
+}
+public String getTime() {
+	return time;
+}
+public void setTime(String time) {
+	this.time = time;
+}
+}
---
+++ src/main/java/com/hospital/serviceImpl/DateAndTimeInfoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hospital.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DateAndTimeInfoServiceImpl {
+
+}
