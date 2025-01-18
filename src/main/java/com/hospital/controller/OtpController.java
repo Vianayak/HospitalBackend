6919adcd44970e9f881eb3176ef4f8f7abc1d731
vinayak banoth <code.editor>
2025-01-18 20:15:10 +0530

    updated email and phn number validation

--- src/main/java/com/hospital/controller/OtpController.java
+++ src/main/java/com/hospital/controller/OtpController.java
@@ -1,5 +1,12 @@
 package com.hospital.controller;
 
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -40,12 +47,25 @@ public class OtpController {
     }
 
     // Method to validate email format using regex
-    private boolean isValidEmail(String email) {
+	public boolean isValidEmail(String email) {
         try {
             InternetAddress emailAddr = new InternetAddress(email);
-            emailAddr.validate();  // This will throw an exception if the email is invalid
-            return true;
+            emailAddr.validate();
+            String domain = email.substring(email.indexOf("@") + 1);
+            return hasMXRecords(domain);
         } catch (AddressException ex) {
+            return false;
+        }
+    }
+
+    private boolean hasMXRecords(String domain) {
+        try {
+            Properties env = new Properties();
+            env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+            DirContext dirContext = new InitialDirContext(env);
+            Attributes attributes = dirContext.getAttributes(domain, new String[]{"MX"});
+            return attributes.get("MX") != null;
+        } catch (NamingException ex) {
             return false;
         }
     }
